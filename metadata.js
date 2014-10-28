05f8001c5e6a217b81ad8a6ab5be08b56b20dcf8
javiber <javier.berneche@gmail.com>
2014-10-28 14:06:52 -0200

    Fix total count metadata was observing the wrong cursor also do some cleanup in the onStop callback

--- metadata.js
+++ metadata.js
@@ -3,57 +3,97 @@ Metadata = {
   add: function (metadata) {
     this.metadatas[metadata.userId] = this.metadatas[metadata.userId]  || {};
     var old = this.metadatas[metadata.userId][metadata.name];
+    this.metadatas[metadata.userId][metadata.name] = metadata;
+
     if (old) {
       //update finalCursor count
       if (this.published[metadata.userId]) {
-        this.published[metadata.userId].changed("CollectionsMetadata", metadata.name, {count: metadata.finalCursor.count()});
+        this.updatePublish(metadata, old);
       }
     }else {
       if (this.published[metadata.userId]){
         this.publish(metadata);
       }
     }
-    this.metadatas[metadata.userId][metadata.name] = metadata;
   },
   get: function(userId){
     return this.metadatas[userId];
   },
   publish: function(metadata){
-    var originalCursor = metadata.cursor;
-    if (originalCursor) {
+    var self = this,
+      finalCursor = metadata.finalCursor;
+    if (finalCursor) {
 
-      var pub = this.published[metadata.userId];
+      var pub = self.published[metadata.userId];
       var initializing = true;
 
-      var handler = originalCursor.observeChanges({
+      var handler = finalCursor.observeChanges({
         added: function (id) {
           //avoid to notify all added changes when it starts
           if (!initializing)
-            pub.changed("CollectionsMetadata", metadata.name, {count: originalCursor.count()});
+            pub.changed("CollectionsMetadata", metadata.name, {count: finalCursor.count()});
         },
         removed: function (id) {
-          pub.changed("CollectionsMetadata", metadata.name, {count: originalCursor.count()});
+          pub.changed("CollectionsMetadata", metadata.name, {count: finalCursor.count()});
         }
       });
+
+      self.metadatas[metadata.userId][metadata.name].handler = handler;
 
       initializing = false;
       //send initial values
 
       pub.added("CollectionsMetadata", metadata.name, {
-        count: originalCursor.count(),
+        count: finalCursor.count(),
         pageSize: metadata.pageSize,
         infiniteScroll: metadata.infiniteScroll
       });
 
       pub.ready();
       pub.onStop(function () {
-        handler.stop();
+        console.log('onStop');
+        self.metadatas[metadata.userId][metadata.name].handler.stop();
+
+        delete self.metadatas[metadata.userId][metadata.name];
+        if (_.isEmpty(self.metadatas[metadata.userId])){
+          delete self.metadatas[metadata.userId];
+        }
       });
     }
   },
+  updatePublish: function (metadata, old) {
+    if (old.handler){
+      old.handler.stop();
+    };
+    var self = this,
+      finalCursor = metadata.finalCursor;
+    if (finalCursor) {
+
+      var pub = this.published[metadata.userId];
+      var initializing = true;
+
+      self.metadatas[metadata.userId][metadata.name].handler = finalCursor.observeChanges({
+        added: function (id) {
+          //avoid to notify all added changes when it starts
+          if (!initializing)
+            pub.changed("CollectionsMetadata", metadata.name, {count: finalCursor.count()});
+        },
+        removed: function (id) {
+          pub.changed("CollectionsMetadata", metadata.name, {count: finalCursor.count()});
+        }
+      });
+      initializing = false;
+      self.published[metadata.userId].changed("CollectionsMetadata", metadata.name, {count: metadata.finalCursor.count()});
+
+    }
+
+
+  },
   setAsPublished: function(pub){
-    this.published = this.published || {};
-    this.published[pub.userId] = pub;
+    var self = this;
+
+    self.published = self.published || {};
+    self.published[pub.userId] = pub;
   }
 };
 
