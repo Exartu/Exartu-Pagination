f71aef0a45f412fef7c0638be2ec1eecbb612c3c
n-campos <ncampos@tempworks.com>
2015-01-21 16:32:13 -0200

    push version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aida:pagination',
   summary: "pagination package from Uruworks",
-  version: "0.1.2",
+  version: "0.1.3",
   git: "https://github.com/Exartu/Exartu-Pagination.git"
 });
 
