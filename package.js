c27c349ecf3b69585e1e5e2a263376a05edaf548
visualaram <aram@seintco.com>
2015-02-11 15:08:11 -0200

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aida:pagination',
   summary: "pagination package from Uruworks",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/Exartu/Exartu-Pagination.git"
 });
 
