c57f34c8f5cbde64efee97abcb03c6405465d186
javier <javier.berneche@gmail.com>
2015-05-08 13:12:17 -0300

    push version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aida:pagination',
   summary: "pagination package from Uruworks",
-  version: "1.0.8",
+  version: "1.0.9",
   git: "https://github.com/Exartu/Exartu-Pagination.git"
 });
 
