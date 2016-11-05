575fd51e8da5fe426f243f386b3852be5557872a
gregg dourgarian <greggd@tempworks.com>
2016-11-05 10:35:23 -0500

    move go-to control to the right

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aida:pagination',
   summary: "pagination package from Uruworks",
-  version: "1.0.15",
+  version: "1.0.16",
   git: "https://github.com/Exartu/Exartu-Pagination.git"
 });
 
@@ -9,11 +9,10 @@ Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@0.9.2');
   api.use(['templating','underscore', 'deps'],'client');
   api.use('aida:mongo-view@0.1.2','server',{ weak: true });
-  
+
   api.addFiles(['metadata.js'], 'server');
   api.addFiles(['server.js'], 'server');
   api.addFiles(['client.js'], 'client');
   api.addFiles(['view.html'], 'client');
   api.addFiles(['view.js'], 'client');
 });
-
